5e454f46a4b82bb7e28da7de5309f6443692d65b
rivaldofez <rivaldofez@gmail.com>
2023-04-23 17:49:17 +0700

    add api url and endpoint

---
+++ app/src/main/java/com/rivaldofez/jetpackcomposeretrofit/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.rivaldofez.jetpackcomposeretrofit.util
+
+class Constants  {
+    companion object {
+        const val BASE_URL = "https://www.freetogame.com/api/"
+        const val GAMES_ENDPOINT = "games"
+    }
+}
